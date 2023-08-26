bf8674441ddfb52a7d1c9e36638f869f14ff29a3
John Williams <john.wrinch.williams@gmail.com>
2023-08-26 14:28:33 -0400

    voice recorder working

--- client/src/HomePage.jsx
+++ client/src/HomePage.jsx
@@ -1,13 +1,47 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { AudioRecorder } from 'react-audio-voice-recorder';
 
 
 const HomePage = () => {
 
+    const addAudioElement = (blob) => {
+        // const url = URL.createObjectURL(blob);
+        // const audio = document.createElement('audio');
+        // audio.src = url;
+        // audio.controls = true;
+        // document.body.appendChild(audio);
+        console.log("blob", blob);
+    };
 
   return (
     <>
-      Home Page
+    HomePage!
+          {/* <AudioRecorder
+        onRecordingComplete={addAudioElement}
+        audioTrackConstraints={{
+          noiseSuppression: true,
+          echoCancellation: true,
+          // autoGainControl,
+          // channelCount,
+          // deviceId,
+          // groupId,
+          // sampleRate,
+          // sampleSize,
+        }}
+        onNotAllowedOrFound={(err) => console.table(err)}
+        downloadOnSavePress={true}
+        downloadFileExtension="webm"
+        mediaRecorderOptions={{
+          audioBitsPerSecond: 128000,
+        }}
+        // showVisualizer={true}
+      /> */}
+      <AudioRecorder
+        onRecordingComplete={addAudioElement} 
+        downloadOnSavePress={true}
+        downloadFileExtension="webm" 
+      />
     </>
   )
 }
